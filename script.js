db8464cfbe2391f50dac6cc2d3528378b326cca1
NandhiniDevi-p <150944181+NandhiniDevi-p@users.noreply.github.com>
2023-12-20 21:52:00 +0530

    Add files via upload
    completed!!

---
+++ script.js
@@ -0,0 +1,38 @@
+setTimeout(() => {
+    document.body.innerHTML = "10"
+    setTimeout(() => {
+        document.body.innerHTML = "9";
+        setTimeout(() => {
+            document.body.innerHTML = "8";
+            setTimeout(() => {
+                document.body.innerHTML = "7";
+                setTimeout(() => {
+                    document.body.innerHTML = "6";
+                    setTimeout(() => {
+                        document.body.innerHTML = "5";
+                        setTimeout(() => {
+                            document.body.innerHTML = "4";
+                            setTimeout(() => {
+                                document.body.innerHTML = "3";
+                                setTimeout(() => {
+                                    document.body.innerHTML = "2";
+                                    setTimeout(() => {
+                                        document.body.innerHTML = "1";
+                                        setTimeout(() => {
+                                            document.body.innerHTML = `
+                                            <h3 style =  "
+                                            text-shadow: 0 0 5px #FFF, 0 0 10p;  
+                                            ">
+                                            <span style = "color: orange" >Happy</span> Independence <span style = "color: green">Day!</span>
+                                            </h3>`;
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
